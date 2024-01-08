ec4aadec5d7db2892eaa917403bb2e3e48060db0
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-01-08 11:05:06 +0100

    Save

--- KoPapirOllo.py
+++ KoPapirOllo.py
@@ -1,6 +1,39 @@
+import threading
+import time
+
 class KoPapirOllo:
+    event = threading.Event()
+    subs: threading = []
+    x = 0
+
+    ido: int = 0
+    gepMutat: int = 0
+    jatekosMutat: int = 0
+    eredmeny: bool = False
+    visszaSzamlalas: int = 3
+    jatekosok = []
 
     def __init__(self):
+        th0 = threading.Thread(target=self.eventLoop())
+        th0.start()
         pass
+    def indit(self):
+        pass
+    def mutat(self, mit: int):
+        if (self.visszaSzamlalas > 0):
+            self.jatekosMutat = mit
+    def getEredmeny(self):
+        return self.eredmeny
 
-    pass
+    def eventLoop(self):
+        while True:
+            self.event.set()
+            time.sleep(1)
+
+    def myFunction(self):
+        while True:
+            self.event.wait()
+            if self.event.is_set():
+                print("event is set : " + str(self.x))
+                self.x += 1
+                self.event.clear()
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 import time
+import threading
+
+global ido
+
 
 if __name__ == '__main__':
     #greeting = tk.Label(text="Hello, Tkinter")
-    window = tk.Tk()
-    greeting = tk.Label(text="Hello, Tkinter")
-    greeting.pack()
-    window.mainloop()
+    #window = tk.Tk()
+    #greeting = tk.Label(text="Hello, Tkinter")
+    #greeting.pack()
+    #window.mainloop()
+    pass
+
+
+
 
